970264e33da43fe06b254b4d149780625da49803
Elia Cohen <mrc6113@rit.edu>
2025-07-19 17:34:32 -0400

    no more base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/portfolio-2025",
-  assetPrefix: "/portfolio-2025",
 };
 
 export default nextConfig;
