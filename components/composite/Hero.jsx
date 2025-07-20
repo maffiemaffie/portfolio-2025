89bef958d697f740032ac1ff78805aba0fc5078e
Elia Cohen <mrc6113@rit.edu>
2025-07-19 23:30:43 -0400

    stars

--- components/composite/Hero.jsx
+++ components/composite/Hero.jsx
@@ -1,20 +1,26 @@
-import styles from '@/styles/composite/Hero.module.scss';
-import { LinedBox } from '../containers';
-import { MiddleHighlight } from '../typography';
+import styles from "@/styles/composite/Hero.module.scss";
+import { LinedBox } from "../containers";
+import { MiddleHighlight } from "../typography";
+import { StarIcon } from "../icons";
 
 export function Hero() {
   return (
-    <LinedBox bigCorners backgroundColor={"background"} borderColor={"primary"} className={styles['hero']}>
+    <LinedBox
+      bigCorners
+      backgroundColor={"background"}
+      borderColor={"primary"}
+      className={styles["hero"]}
+    >
       <h1>
         <MiddleHighlight>
+          <StarIcon large filled/>
           Port
           <br />
           folio
+          <StarIcon large filled/>
         </MiddleHighlight>
       </h1>
-      <span className={styles['year']}>
-        2025
-      </span>
+      <span className={styles["year"]}>2025</span>
     </LinedBox>
   );
-};
+}
