f5765460413ee2a45b5ed377b9e32905dafcfaa4
Elia Cohen <mrc6113@rit.edu>
2025-07-20 16:48:24 -0400

    animated cover

--- components/containers/Box.jsx
+++ components/containers/Box.jsx
@@ -1,12 +1,21 @@
 import colors from "@/styles/color-classes.module.scss";
 import styles from "@/styles/containers/Box.module.scss";
 
-export function Box({ children, backgroundColor, className, bigCorners }) {
+export function Box({
+  children,
+  backgroundColor,
+  className,
+  bigCorners,
+  onMouseEnter = () => {},
+  onMouseLeave = () => {},
+}) {
   const backgroundClass =
     colors[`background-${backgroundColor}`] ?? colors["background-custom"];
 
   return (
     <div
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
       className={`
         ${backgroundClass} 
         ${styles.box} 
