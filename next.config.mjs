1804dc1394a100b7896e98c3a001056c315b2fa0
Elia Cohen <mrc6113@rit.edu>
2025-07-20 13:58:25 -0400

    debug aids

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    styledComponents: {
+      displayName: true, // helps debugging
+      ssr: true,
+      fileName: true,
+      minify: false // ← this disables minification
+    },
+  }
 };
 
 export default nextConfig;
